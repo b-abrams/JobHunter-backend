70b9832813a514fc76f1db32d40e3aba0aca5970
Breeland Abrams <breeland.abrams@gmail.com>
2019-05-05 12:26:27 -0700

    copy files from Job-Listing-Web-Scraper

---
+++ JobSearch.py
@@ -0,0 +1,59 @@
+
+from Job import Job
+import IndeedSearch
+import webbrowser
+import requests
+from googlesearch import search
+from bs4 import BeautifulSoup
+import json
+
+
+
+#Take jobs retrieved from search and format them as dicts using serialize
+def jobFormat(jobs: list):
+    serialized = []
+    for i in range(len(jobs)):
+        serialized.append(jobs[i].serialize())
+    return serialized
+
+# Execute a job search using a given query string
+"""
+Example
+
+query = Software Engineering Internship
+
+1. Google search is conducted for "Software Engineering Internship"
+
+2. First indeed and glassdoor results are saved. (only indeed link is used for now).
+
+3. IndeedSearch.py is executed and a list of jobs is returned
+
+"""
+def execute(query: str):
+    indeedLink = ""
+    glassdoorLink = ""
+    for x in search(query, tld='com', lang='en', num=10, stop=10, pause=1):
+        if(len(indeedLink) > 1 and len(glassdoorLink) > 1):
+            break
+        if("indeed" in x):
+            if(len(indeedLink) > 1):
+                continue
+            indeedLink = x
+        if("glassdoor" in x):
+            if(len(glassdoorLink) > 1):
+                continue
+            glassdoorLink = x
+    indeed = IndeedSearch.execute(indeedLink)
+    return jobFormat(indeed)
+
+if __name__ == "__main__":
+    # print(execute(input("Enter Seacrch Query:\n")))
+    f = open("test.json", "w+")
+    jsonList = []
+    for _ in list(execute("Software Engineering Internship")):
+        if(_ not in jsonList):
+          jsonList.append(_)
+    jobs = json.dumps(jsonList[:9])
+    f.write(jobs)
+    f.close()
+    
